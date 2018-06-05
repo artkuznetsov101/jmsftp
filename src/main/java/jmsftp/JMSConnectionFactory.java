7e3f8b83d9cc2ec9b74c9809ae6df92d162ac3a0
art <artkuznetsov101@gmail.com>
2018-06-05 21:42:03 +0300

    fix

--- src/main/java/jmsftp/JMSConnectionFactory.java
+++ src/main/java/jmsftp/JMSConnectionFactory.java
@@ -14,15 +14,15 @@ public class JMSConnectionFactory {
 		JmsFactoryFactory ff = JmsFactoryFactory.getInstance(WMQConstants.WMQ_PROVIDER);
 		JmsConnectionFactory factory = ff.createConnectionFactory();
 
-		factory.setStringProperty(WMQConstants.WMQ_HOST_NAME, Settings.JMS.HOST);
-		factory.setStringProperty(WMQConstants.WMQ_PORT, Settings.JMS.PORT);
-		factory.setStringProperty(WMQConstants.WMQ_CHANNEL, Settings.JMS.CHANNEL);
+		factory.setStringProperty(WMQConstants.WMQ_HOST_NAME, Config.JMS.HOST);
+		factory.setStringProperty(WMQConstants.WMQ_PORT, Config.JMS.PORT);
+		factory.setStringProperty(WMQConstants.WMQ_CHANNEL, Config.JMS.CHANNEL);
 		factory.setIntProperty(WMQConstants.WMQ_CONNECTION_MODE, WMQConstants.WMQ_CM_CLIENT);
-		factory.setStringProperty(WMQConstants.WMQ_QUEUE_MANAGER, Settings.JMS.QUEUE_MANAGER);
+		factory.setStringProperty(WMQConstants.WMQ_QUEUE_MANAGER, Config.JMS.QUEUE_MANAGER);
 		factory.setStringProperty(WMQConstants.WMQ_APPLICATIONNAME, "jms ftp bridge");
 		factory.setBooleanProperty(WMQConstants.USER_AUTHENTICATION_MQCSP, true);
-		factory.setStringProperty(WMQConstants.USERID, Settings.JMS.USER);
-		factory.setStringProperty(WMQConstants.PASSWORD, Settings.JMS.PASSWORD);
+		factory.setStringProperty(WMQConstants.USERID, Config.JMS.USERNAME);
+		factory.setStringProperty(WMQConstants.PASSWORD, Config.JMS.PASSWORD);
 
 		return factory;
 	}
